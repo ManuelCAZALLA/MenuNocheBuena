45b225d1e5eacb9ad9c7e35bafe07e8fdf6d59f5
ManuelCAZALLA <manuelcm23@hotmail.com>
2024-12-16 20:13:33 +0100

    Generada URL pública utilizando ngrok para compartir el menú desde cualquier dispositivo móvil. - Configuración de CORS y cambios en  para aceptar conexiones externas. - Ajuste de puertos para evitar conflictos y asegurar funcionalidad en macOS.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
+
 from flask_cors import CORS
 CORS(app)
 
@@ -13,8 +14,10 @@ frases = [
     "Abuela te acuerdas aquella vez que por mas que rezabas para llegar a casa sin cagarte, no hubo ayuda divina?.",
     "Abuelo es cierto que no te comiste el plato de sopa, porque no tenías cuchara?.",
     "Abuela te acuerdas cuando mandaste al abuelo a por un poco de huevo hilado? Compro 2 kilos.",
-    "Abuela te acuerda cuando fuiste a los Villares y andastes mas que en el reto Araque?"
+    "Abuela te acuerda cuando fuiste a los Villares y andastes mas que en el reto Araque?",
     "Os acordais la navidad tan buena que pasó el cura al llevarse el jamon de 200 euros"
+    "Abuelo te acuerdas cuando tiraste al Paquito el Sigal porque no paraba de cantar y te tenias que ir a misa?"
+    "Abuelo te acuerdas cuando tocaste las campanas a pino después de comer? Corria mas que Mbappe para la iglesia"
 ]
 
 @app.route('/')
